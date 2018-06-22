777cfc7e5dfea22941653f3718173c331a81ed4c
Danya Generalov <danya02@me.com>
2018-06-22 13:37:09 +0300

    Adding pygame.midi I/O interfaces.

---
+++ pygame_io.py
@@ -0,0 +1,39 @@
+import pygame
+import pygame.midi
+import common
+
+
+class PygameInput(common.Input):
+    """Interface to Input of pygame.midi."""
+
+    def __init__(self, id: int):
+        self.input = pygame.midi.Input(id)
+
+    def poll(self):
+        return self.input.poll()
+
+    def get(self, count: int = 1, block: bool = False):
+        buffer = []
+        while len(buffer) != count:
+            buffer += self.input.read(1)
+            if not block and not self.poll():
+                break
+        messages = []
+        for i in buffer:
+            m = common.MIDIMessage()
+            m.status = i[0][0]
+            m.data = i[0][1:]
+            messages += [m]
+        return messages
+
+
+class PygameOutput(common.Output):
+    """Interface to Output of pygame.midi."""
+
+    def __init__(self, id: int):
+        self.output = pygame.midi.Output(id, latency=0)
+
+    def put(self, message: common.MIDIMessage, callback: callable = None):
+        self.output.write([[message.status] + message.data, pygame.midi.time()])
+        callback(message)
+
